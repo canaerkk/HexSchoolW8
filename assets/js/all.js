2f34384340d28d592c54d130d19b5dfe92c5144a
Paisley <paisley.chien@gmail.com>
2021-08-29 23:51:46 +0800

    Update 2021-08-29T15:51:44.169Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,20 +5,21 @@
   'use strict'; // breakpoint where swiper will be destroyed
   // and switches to a dual-column layout
 
-  var breakpoint = window.matchMedia('(max-width: 992px)'); // keep track of swiper instances to destroy later
+  var lgbreakpoint = window.matchMedia('(max-width: 992px)'); // keep track of swiper instances to destroy later
 
-  var mySwiper; //////////////////////////////////////////////////////////////////
+  var mySwiper;
+  var listenSwiper; //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
 
   var breakpointChecker = function breakpointChecker() {
     // if larger viewport and multi-row layout needed
-    if (breakpoint.matches === true) {
+    if (lgbreakpoint.matches === true) {
       // clean up old instances and inline styles when available
       if (mySwiper !== undefined) mySwiper.destroy(true, true); // or/and do nothing
 
-      return; // else if a small viewport and single column layout needed
-    } else if (breakpoint.matches === false) {
+      return;
+    } else if (lgbreakpoint.matches === false) {
       // fire small viewport version of swiper
       return enableSwiper();
     }
@@ -32,16 +33,46 @@
       slidesPerView: 1.5,
       spaceBetween: 30
     });
-    console.log('swiper');
+    console.log('mySwiper');
+  };
+
+  var enableListenSwiper = function enableListenSwiper() {
+    listenSwiper = new Swiper(".listenSwiper", {
+      slidesPerView: 1,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      grid: {
+        rows: 3,
+        fill: 'column'
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2.1,
+          grid: {
+            rows: 2
+          }
+        },
+        1200: {
+          slidesPerView: 3,
+          grid: {
+            rows: 2
+          }
+        }
+      }
+    });
+    console.log('listenSwiper');
   }; //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
   // keep an eye on viewport size changes
 
 
-  breakpoint.addListener(breakpointChecker); // kickstart
+  lgbreakpoint.addListener(breakpointChecker); // kickstart
 
   breakpointChecker();
+  enableListenSwiper();
 })();
 /* IIFE end */
 //# sourceMappingURL=all.js.map
